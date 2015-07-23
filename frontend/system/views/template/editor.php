4769ae7c89a292b1613f8e58b002c7cd7784f2b2
jishengjun <shengjun@zamplus.com>
2015-07-23 13:05:48 +0800

    1

--- frontend/system/views/template/editor.php
+++ frontend/system/views/template/editor.php
@@ -4,7 +4,10 @@
  * Author: Shengjun
  * CreateTime: 15-7-23.上午1:17
  * Description:
- */
+ */ 
+
+$imagesWhereUrl = "";
+
 $js=<<<JS
 var data = {
         "1": {
@@ -38,14 +41,19 @@ var data = {
             }
         }
     };
-    var data = {};
+    // 这是前一次数据保存的数据。以便继续操作
+	var data = {};
+	
+	
+
     //try{
-    ZSYFCEditor.init(
+    ZSYFCEditor.config( { "imagesWhereUrl":  "$imagesWhereUrl" } );
+	ZSYFCEditor.init(
         data,
         {
             svg: d3.select("svg.ZSYFCEditor"),
-            width: 1000,
-            height: 800
+            width: 900,
+            height: 560
         } );
 JS;
 
@@ -59,23 +67,20 @@ $this->registerJsFile('/js/ZSYFCEditor.js',['depends'=>'frontend\assets\AppAsset
 $this->registerCssFile('/css/popuppanel.css');
 $this->registerCssFile('/css/style.css');
 ?>
-<div style="width: 1000px;height:850px; margin: 0; padding: 0;">
-<div class="ZSYFCEditor-btnbar">
-    <span class="btn mainSwitch" onclick="ZSYFCEditor.addShape('mainSwitch')" title="聚汇交换机"><img src="/images/icons/switch1.png" height="25" ></span>
-    <span class="btn switch" onclick="ZSYFCEditor.addShape('switch')" title="交换机"><img src="/images/icons/switch2.png" height="25" ></span>
-    <span class="btn server" onclick="ZSYFCEditor.addShape('server')" title="服务器"><img src="/images/icons/server.png" height="25" ></span>
-    <span class="btn db" onclick="ZSYFCEditor.addShape('db')" title="数据库服务器"><img src="/images/icons/db.png" height="25" ></span>
-    <span class="btn firewall" onclick="ZSYFCEditor.addShape('firewall')" title="防火墙"><img src="/images/icons/firewall.png" height="25" ></span>
-</div>
-<svg class="ZSYFCEditor" oncontextmenu="return false;" >
-    <defs>
-        <marker id="ZSYFCEditor_MarkerArrow" markerWidth="13" markerHeight="13" refx="9" refy="6" orient="auto">
-            <path d="M2,2 L2,11 L10,6 L2,2" style="fill: #000000;" />
-        </marker>
-    </defs>
-    <g class="container"></g>
-</svg>
-</div>
-<script>
-
-</script>
+<div style="width: 900px;height:620px; margin: 0; padding: 0;">
+	<div class="ZSYFCEditor-btnbar">
+		<span class="btn mainSwitch" onclick="ZSYFCEditor.addShape('mainSwitch')" title="聚汇交换机"><img src="/images/icons/switch1.png" height="25" ></span>
+		<span class="btn switch" onclick="ZSYFCEditor.addShape('switch')" title="交换机"><img src="/images/icons/switch2.png" height="25" ></span>
+		<span class="btn server" onclick="ZSYFCEditor.addShape('server')" title="服务器"><img src="/images/icons/server.png" height="25" ></span>
+		<span class="btn db" onclick="ZSYFCEditor.addShape('db')" title="数据库服务器"><img src="/images/icons/db.png" height="25" ></span>
+		<span class="btn firewall" onclick="ZSYFCEditor.addShape('firewall')" title="防火墙"><img src="/images/icons/firewall.png" height="25" ></span>
+	</div>
+	<svg class="ZSYFCEditor" oncontextmenu="return false;" >
+		<defs>
+			<marker id="ZSYFCEditor_MarkerArrow" markerWidth="13" markerHeight="13" refx="9" refy="6" orient="auto">
+				<path d="M2,2 L2,11 L10,6 L2,2" style="fill: #000000;" />
+			</marker>
+		</defs>
+		<g class="container"></g>
+	</svg>
+</div> 
