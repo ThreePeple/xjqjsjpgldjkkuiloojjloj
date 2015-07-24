70dfdef3bcacc2243e95189d27e0829197ef5073
jim-jim-jim <757109436@qq.com>
2015-07-24 11:47:25 +0800

    Format code.

--- frontend/web/js/app.js
+++ frontend/web/js/app.js
@@ -1,11 +1,13 @@
-function renderHub(dataUrl,containerId,detailUrl){
+function renderHub(dataUrl, containerId, detailUrl) {
     var radius = 800 / 2; // SVG radius
 
     var cluster = d3.layout.cluster()
         .size([360, radius - 250]);
 
     var diagonal = d3.svg.diagonal.radial()
-        .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+        .projection(function(d) {
+            return [d.y, d.x / 180 * Math.PI];
+        });
 
     var svg = d3.select("#" + containerId).append("svg")
         .attr("width", radius * 2)
@@ -13,20 +15,20 @@ function renderHub(dataUrl,containerId,detailUrl){
         .append("g")
         .attr("transform", "translate(" + radius + "," + radius + ")");
 
-    var _getNodeDetailTpl = function (){
+    var _getNodeDetailTpl = function() {
         var tmp = null;
-        return function (){
-            if( !tmp ){
+        return function() {
+            if (!tmp) {
                 tmp = $('#switch_node_detail').html();
             }
             return tmp;
         };
     }();
 
-    var _showNodeDetail = function( d, e, contentHtmlTpl ){
+    var _showNodeDetail = function(d, e, contentHtmlTpl) {
         console.log(d, e, contentHtmlTpl)
         var $tg = $(e.target);
-        if( false == $tg.is("g.box") ){
+        if (false == $tg.is("g.box")) {
             $tg = $tg.closest("g.box");
         }
         $tg = $tg.find("circle");
@@ -43,19 +45,19 @@ function renderHub(dataUrl,containerId,detailUrl){
         var offsetX = 0,
             offsetY = 0;
         var className = "nodeDetail";
-        var _parseData = function ( data ){
-            if( data ){
-                for( var p in data ){
-                    contentHtmlTpl = contentHtmlTpl.replace( '{' + p + '}', data[p] );
+        var _parseData = function(data) {
+            if (data) {
+                for (var p in data) {
+                    contentHtmlTpl = contentHtmlTpl.replace('{' + p + '}', data[p]);
                 }
                 return contentHtmlTpl;
             }
             return "<span class='none'>No data.</span>";
         };
-        var _updateContent = function( html ){
-            return 	contentTpl.replace( "{content}", html );
+        var _updateContent = function(html) {
+            return contentTpl.replace("{content}", html);
         };
-        var popPanel =  new PopupPanel({
+        var popPanel = new PopupPanel({
             className: 'modalessDialog' + (className ? " " + className : ""),
             offsetX: offsetX,
             offsetY: offsetY,
@@ -66,21 +68,23 @@ function renderHub(dataUrl,containerId,detailUrl){
             initInterface: function($content) {
                 var inst = this;
                 $content.click(function(e) {
-                    if( $(e.target).is('div.popup_close'))
+                    if ($(e.target).is('div.popup_close'))
                         inst.close(true);
                 });
 
                 $content.find("div.popup_content").html(_updateContent("loading..."));
                 console.log(d)
                 // nodeDetail_.json : fail data, nodeDetail.json: ok data.
-                $.get( detailUrl, { node: d.id }, function(j){
-                    if( j.result == 1 ){
-                        $content.find("div.popup_content").html(_updateContent(_parseData( j.data )));
+                $.get(detailUrl, {
+                    node: d.id
+                }, function(j) {
+                    if (j.result == 1) {
+                        $content.find("div.popup_content").html(_updateContent(_parseData(j.data)));
                     } else {
-                        $content.find("div.popup_content").html(_updateContent( j.msg ));
+                        $content.find("div.popup_content").html(_updateContent(j.msg));
                     }
                     popPanel.refresh();
-                }, 'json' );
+                }, 'json');
             }
         }).init().show(x, y);
     };
@@ -88,20 +92,20 @@ function renderHub(dataUrl,containerId,detailUrl){
         if (error) throw error;
 
         var nodes = cluster.nodes(root);
-        var link =  svg.selectAll("path.link")
+        var link = svg.selectAll("path.link")
             .data(cluster.links(nodes))
             .enter().append("path")
-            .attr("class", function( d ){
-                if( d['target']['status'] == 0 )
+            .attr("class", function(d) {
+                if (d['target']['status'] == 0)
                     return "link error";
                 return "link";
             })
-            .attr("d", diagonal) ;
+            .attr("d", diagonal);
 
-        var  _path  = function( node ){
+        var _path = function(node) {
             var len = link[0].length;
-            for( var i = 0 ; i < len; i++){
-                if( d3.select(link[0][i]).data()[0].target == d3.select(node).data()[0] ){
+            for (var i = 0; i < len; i++) {
+                if (d3.select(link[0][i]).data()[0].target == d3.select(node).data()[0]) {
                     return d3.select(link[0][i]);
                 }
             }
@@ -111,33 +115,35 @@ function renderHub(dataUrl,containerId,detailUrl){
         var node = svg.selectAll("g.node")
             .data(nodes)
             .enter().append("g")
-            .attr("class", function(d){
-                if( d['status'] == 0 )
+            .attr("class", function(d) {
+                if (d['status'] == 0)
                     return "node error";
                 return "node";
             })
-            .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; });
+            .attr("transform", function(d) {
+                return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")";
+            });
 
 
         node = node.append("g")
-            .attr( "class", "box" )
-            .on("mouseover", function (){
-                _path(this.parentNode).classed( "hover", true );
+            .attr("class", "box")
+            .on("mouseover", function() {
+                _path(this.parentNode).classed("hover", true);
             })
-            .on("mouseout", function (){
-                _path(this.parentNode).classed( "hover", false );
+            .on("mouseout", function() {
+                _path(this.parentNode).classed("hover", false);
             })
-            .on( "click", function( data ){
-                _showNodeDetail( data, d3.event, _getNodeDetailTpl() );
+            .on("click", function(data) {
+                _showNodeDetail(data, d3.event, _getNodeDetailTpl());
             });
 
         node.append("rect")
-            .attr("x","-5")
+            .attr("x", "-5")
             .attr("y", "-5")
             .attr("width", "30px")
             .attr("height", "10px")
-            //.attr("fill", "red"); // Fixed: g.box:hover 遇到空隙会闪烁
-            .attr("fill", "transparent");
+        //.attr("fill", "red"); // Fixed: g.box:hover 遇到空隙会闪烁
+        .attr("fill", "transparent");
 
         node.append("circle")
             .attr("cx", "0.15em")
@@ -145,10 +151,18 @@ function renderHub(dataUrl,containerId,detailUrl){
 
         node.append("text")
             .attr("dy", ".31em")
-            .attr("text-anchor", function(d) { return d.x < 180 ? "start" : "end"; })
-            .attr("level", function(d) { return d.depth; })
-            .attr("transform", function(d) { return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)"; })
-            .text(function(d) { return d.name; }) ;
+            .attr("text-anchor", function(d) {
+                return d.x < 180 ? "start" : "end";
+            })
+            .attr("level", function(d) {
+                return d.depth;
+            })
+            .attr("transform", function(d) {
+                return d.x < 180 ? "translate(8)" : "rotate(180)translate(-8)";
+            })
+            .text(function(d) {
+                return d.name;
+            });
     });
 
     d3.select(self.frameElement).style("height", radius * 2 + "px");
