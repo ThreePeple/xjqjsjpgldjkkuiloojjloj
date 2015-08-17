96390758041bc4397c90390cc1fffde889f1ec95
jsj <shengjun@zamplus.com>
2015-08-17 18:00:05 +0800

    menu update

--- frontend/views/layouts/header.php
+++ frontend/views/layouts/header.php
@@ -5,6 +5,18 @@ use yii\bootstrap\NavBar;
 
 /* @var $this \yii\web\View */
 /* @var $content string */
+
+$css = <<<CSS
+        .dropdown:hover .menu-top{display:block;}
+        .dropdown-submenu{position:relative;}
+        .dropdown-submenu > .dropdown-menu{top:0;left:100%;margin-top:-6px;margin-left:-1px;-webkit-border-radius:0 6px 6px 6px;-moz-border-radius:0 6px 6px 6px;border-radius:0 6px 6px 6px;}
+        .dropdown-submenu:hover > .dropdown-menu{display:block;}
+        .dropdown-submenu > a:after{display:block;content:" ";float:right;width:0;height:0;border-color:transparent;border-style:solid;border-width:5px 0 5px 5px;border-left-color:#cccccc;margin-top:5px;margin-right:-10px;}
+        .dropdown-submenu:hover > a:after{border-left-color:#ffffff;}
+        .dropdown-submenu .pull-left{float:none;}
+        .dropdown-submenu.pull-left > .dropdown-menu{left:-100%;margin-left:10px;-webkit-border-radius:6px 0 6px 6px;-moz-border-radius:6px 0 6px 6px;border-radius:6px 0 6px 6px;}
+CSS;
+$this->registerCss($css);
 ?>
 
 <?php
@@ -19,26 +31,40 @@ NavBar::begin([
     ]
 ]);
 $active = Yii::$app->controller->module->getUniqueId();
+
+$action = Yii::$app->controller->action->getUniqueId();
+
 $menuItems = [
     [
         'label' => '拓扑展示',
         'items' =>[
             [
-                'label' => '大厦局域网',
+                'label' => '大厦拓扑概览',
                 'url' => ['/topology/dashboard/index'],
                 'active' => ""
             ],
+            '<li role="separator" class="divider"></li>',
             [
-                'label' => '有线网络',
-                'url' => ['/topology/dashboard/wlan']
+                "label" =>"有线网络",
+                "url" => "/topology/dashboard/wlan",
+                "items" => [
+                    [
+                        "label"=>"大厦局域网1",
+                        "url"=>["/topology/dashboard/wlan"],
+                        "options" => ["class"=>($action == 'topology/dashboard/wlan'? 'active':'')]
+                    ],
+                    [
+                        "label"=>"大厦局域网2",
+                        "url"=>"#",
+                        "options" => ["class"=>($action == 'topology/dashboard/'? 'active':'')]
+                    ]
+                ],
+                'active' => in_array($action,['topology/dashboard/wlan','topology/dashboard/'])
             ],
+            '<li role="separator" class="divider"></li>',
             [
                 'label' => '无线网络',
                 'url' => ['/topology/dashboard/wireless']
-            ],
-            [
-                'label' => '交换机组网',
-                'url' => ['/topology/dashboard/index']
             ],
         ],
         'active' => $active == 'topology'
