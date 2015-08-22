d7450099599621a11a0857e3d0ea48bcc93f1773
jsj <shengjun@zamplus.com>
2015-08-22 15:24:38 +0800

    params update

--- common/config/params.php
+++ common/config/params.php
@@ -3,4 +3,11 @@ return [
     'adminEmail' => 'admin@example.com',
     'supportEmail' => 'support@example.com',
     'user.passwordResetTokenExpire' => 3600,
+    "apiAuth" => [
+        "type" => "http_basic",
+        "http_basic" => [
+            "user" => 'huligen',
+            "pwd" => 'huligen',
+        ]
+    ],
 ];
