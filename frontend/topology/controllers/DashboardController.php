720d5500b1b7f25c36d0a696236c45cc3a74d674
Jim <757109436@qq.com>
2015-08-23 23:49:48 +0800

    .

--- frontend/topology/controllers/DashboardController.php
+++ frontend/topology/controllers/DashboardController.php
@@ -64,7 +64,7 @@ class DashboardController extends Controller
     }
 
     /**
-     * 刷新面板数据
+     * 刷新面板、链路数据
      */
     public function actionAjaxLinksRefresh(){
         $type = Yii::$app->request->post('type');
