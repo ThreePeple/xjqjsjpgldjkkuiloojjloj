7bec17492d9533f31dc4fa918f50c6db8d715e6f
Jim <757109436@qq.com>
2015-08-31 11:16:30 +0800

    下拉菜单优化。

--- frontend/views/layouts/header.php
+++ frontend/views/layouts/header.php
@@ -7,15 +7,21 @@ use yii\bootstrap\NavBar;
 /* @var $content string */
 
 $css = <<<CSS
+        .dropdown-menu{ padding: 1px 0;}
         .dropdown:hover .menu-top{display:block;}
         .dropdown.open a.dropdown-toggle{z-index: 1100;box-shadow: 0 0 8px 0px #fff; background-color: #fff !important; color: #080808 !important; }
         .dropdown-submenu{position:relative;}
+        .dropdown-menu li > a {padding-top: 9px; padding-bottom: 9px; }
+        .dropdown-menu .divider{ margin: 0 !important;}
         .dropdown-menu { box-shadow: 0 0 8px 0px #fff; background-color: rgba(255,255,255,0.987); }
         .dropdown.open .dropdown-menu{ margin-top: -1px; }
         .dropdown-submenu > .dropdown-menu{top:0;left:100%;margin-top:-6px;margin-left:-1px;-webkit-border-radius:0 6px 6px 6px;-moz-border-radius:0 6px 6px 6px;border-radius:0 6px 6px 6px;}
         .dropdown-submenu:hover > .dropdown-menu{display:block;}
         .dropdown-submenu > a:after{display:block;content:" ";float:right;width:0;height:0;border-color:transparent;border-style:solid;border-width:5px 0 5px 5px;border-left-color:#cccccc;margin-top:5px;margin-right:-10px;}
         .dropdown-submenu:hover > a:after{border-left-color:#ffffff;}
+        .dropdown-submenu > .dropdown-menu{ }
+        .dropdown-submenu:hover > .dropdown-menu{ -webkit-animation: open_submenu 0.3s; }
+        @-webkit-keyframes open_submenu { from { margin-left: -20px; opacity: 0;; } 90% { margin-left: 5px; opacity: 0.9; } to { margin-left: 0; opacity: 1; }  }
         .dropdown-submenu .pull-left{float:none;}
         .dropdown-submenu.pull-left > .dropdown-menu{left:-100%;margin-left:10px;-webkit-border-radius:6px 0 6px 6px;-moz-border-radius:6px 0 6px 6px;border-radius:6px 0 6px 6px;}
 CSS;
