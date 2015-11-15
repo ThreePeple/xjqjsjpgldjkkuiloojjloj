1b4cead69142f2cf2eab6f1f3f9f2f980a92f784
jishengjun <shengjun@zamplus.com>
2015-11-15 17:10:48 +0800

    11

--- frontend/topology/views/dashboard/_changeView.php
+++ frontend/topology/views/dashboard/_changeView.php
@@ -14,7 +14,7 @@
     <title></title>
 </head>
 <body>
-<iframe id="container" src="<?=$uris[0]?>" style="width: 100%; height: 1000px"  frameBorder="0">
+<iframe id="container" src="<?=$uris[0]?>" style="width: 100%; height: 1000px"  frameborder=0 marginheight=0 marginwidth=0 scrolling=no>
 
 </iframe>
 <script>
